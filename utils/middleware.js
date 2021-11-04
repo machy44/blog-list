69703ce5aa3eab108c134be38762261de0d12047
alen.sljivar <alen.sljivar@five.agency>
2021-11-04 23:32:58 +0100

    prepare blog-list project

---
+++ utils/middleware.js
@@ -0,0 +1,16 @@
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+};
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+};
