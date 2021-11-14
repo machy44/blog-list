06d5f5f0db33893c81908ef7c3aa83bb83a28820
alen.sljivar <alen.sljivar@five.agency>
2021-11-14 22:55:10 +0100

    finish 4.13-4.14 exercises

--- tests/blog_list_api.test.js
+++ tests/blog_list_api.test.js
@@ -7,21 +7,21 @@ const api = supertest(app);
 
 const Blog = require('../models/blog');
 
-beforeAll(async () => {
+beforeEach(async () => {
   await Blog.deleteMany();
   await Blog.insertMany(helper.initialBlogs);
 });
 
 describe('viewing blogs', () => {
-  // test('blogs are returned as json', async () => {
-  //   await api.get('/api/blogs').expect(200);
-  //   // .expect('Content-Type', /application\/json/);
-  // });
+  test('blogs are returned as json', async () => {
+    await api.get('/api/blogs').expect(200);
+    // .expect('Content-Type', /application\/json/);
+  });
   test('there are two blogs', async () => {
-    const blogsAtEnd = await helper.blogsInDb();
-    // const blogsAtEnd = await api.get('/api/blogs');
+    // const blogsAtEnd = await helper.blogsInDb();
+    const response = await api.get('/api/blogs');
 
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
   });
 
   test('there should be id property', async () => {
@@ -31,58 +31,101 @@ describe('viewing blogs', () => {
 });
 
 describe('addition of a new blog', () => {
-  // test('a valid blog can be added', async () => {
-  //   const newBlog = {
-  //     title: 'newest',
-  //     author: 'alen',
-  //     url: 'this represents url',
-  //     likes: 0,
-  //   };
-  //   await api.post('/api/blogs').send(newBlog);
-  //   // .expect(201);
-  //   // .expect('Content-Type', /application\/json/);
-  //   const blogsAtEnd = await helper.blogsInDb();
-  //   expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
-  //   const titles = blogsAtEnd.map((r) => r.title);
-  //   expect(titles).toContain('newest');
-  // });
-  // test('new blog can be added without likes key which is set to zero by default', async () => {
-  //   const newBlog = {
-  //     title: 'newest',
-  //     author: 'alen',
-  //     url: 'this represents url',
-  //   };
-  //   await api
-  //     .post('/api/blogs')
-  //     .send(newBlog)
-  //     .expect(201)
-  //     .expect('Content-Type', /application\/json/);
-  //   const blogsAtEnd = await helper.blogsInDb();
-  //   const likes = blogsAtEnd.map((r) => r.likes);
-  //   const addedLikeValue = likes[likes.length - 1];
-  //   expect(addedLikeValue).toBe(0);
-  // });
-  // test('blog without title is not added', async () => {
-  //   const newBlog = {
-  //     author: 'alen',
-  //     url: 'url title',
-  //     likes: 2,
-  //   };
-  //   await api.post('/api/blogs').send(newBlog).expect(400);
-  //   const blogsAtEnd = await helper.blogsInDb();
-  //   expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
-  // });
-  // test('blog without url is not added', async () => {
-  //   const newBlog = {
-  //     author: 'alen',
-  //     title: 'test title',
-  //     likes: 2,
-  //   };
-  //   await api.post('/api/blogs').send(newBlog).expect(400);
-  //   const blogsAtEnd = await helper.blogsInDb();
-  //   expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
-  // });
-  test('update blog likes', () => {});
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'newest',
+      author: 'alen',
+      url: 'this represents url',
+      likes: 0,
+    };
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+    const titles = blogsAtEnd.map((r) => r.title);
+    expect(titles).toContain('newest');
+  });
+  test('new blog can be added without likes key which is set to zero by default', async () => {
+    const newBlog = {
+      title: 'newest',
+      author: 'alen',
+      url: 'this represents url',
+    };
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+    const blogsAtEnd = await helper.blogsInDb();
+    const likes = blogsAtEnd.map((r) => r.likes);
+    const addedLikeValue = likes[likes.length - 1];
+    expect(addedLikeValue).toBe(0);
+  });
+  test('blog without title is not added', async () => {
+    const newBlog = {
+      author: 'alen',
+      url: 'url title',
+      likes: 2,
+    };
+    await api.post('/api/blogs').send(newBlog).expect(400);
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+  });
+  test('blog without url is not added', async () => {
+    const newBlog = {
+      author: 'alen',
+      title: 'test title',
+      likes: 2,
+    };
+    await api.post('/api/blogs').send(newBlog).expect(400);
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+  });
+});
+
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+
+    const titles = blogsAtEnd.map((blog) => blog.title);
+
+    expect(titles).not.toContain(blogToDelete.title);
+  });
+  test('not succeeds with status code 400 if id is valid', async () => {
+    await api.delete('/api/blogs/notValidId').expect(400);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+  });
+});
+
+describe('blog update', () => {
+  test('update blog likes', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToUpdate = blogsAtStart[0];
+
+    expect(blogToUpdate.likes).toBe(1);
+
+    blogToUpdate.likes = 2;
+
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(blogToUpdate)
+      .expect(200);
+    expect(blogToUpdate.likes).toBe(2);
+  });
 });
 
 afterAll(() => {
--- utils/middleware.js
+++ utils/middleware.js
@@ -3,13 +3,11 @@ const unknownEndpoint = (request, response) => {
 };
 
 const requestLogger = (request, response, next) => {
-  if (process.env.NODE_ENV !== 'test') {
-    console.log('Method:', request.method);
-    console.log('Path:  ', request.path);
-    console.log('Body:  ', request.body);
-    console.log('---');
-    next();
-  }
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
 };
 
 const errorHandler = (error, request, response, next) => {
